c759825b4c8a481a2973bbe5eb8024c025034641
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-29 22:09:53 -0600

    added show error function

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 // show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message
+}
 
 // event listeners below the functions
 form.addEventListener('submit', function(e){
