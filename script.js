3dc38f2d0a61ab1719244dd4d3bcec8a80d0a216
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-29 22:16:24 -0600

    added regex for valid email

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 
+// check email is valid 
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 // event listeners below the functions
 form.addEventListener('submit', function(e){
   e.preventDefault();
