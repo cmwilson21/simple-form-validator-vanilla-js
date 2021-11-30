7822dc1538814908fe151fee4f22db46a77efda1
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-29 22:18:25 -0600

    added basic validation before refactor

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ form.addEventListener('submit', function(e){
   }
   if(email.value === '') {
     showError(email, 'Email is required');
+  } else if(!isValidEmail(email.value)){
+    showError(email, 'Email is not valid');
   } else {
     showSuccess(username);
   }
