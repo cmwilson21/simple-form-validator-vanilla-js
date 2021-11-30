bcc5a647a04bc411b6edb818f6379573263d1e46
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-29 22:05:03 -0600

    added first event listener

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+// show input error message
+
+// event listeners below the functions
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  if(username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+})
