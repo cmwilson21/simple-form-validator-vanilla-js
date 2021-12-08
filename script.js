c61b16e0930966be0df1453d91ddc2aee22dc0b2
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-12-07 22:49:03 -0600

    refactored to remove the if else and added functions for the input field

--- script.js
+++ script.js
@@ -24,30 +24,49 @@ function isValidEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
+// check required fields - pass in array of inputs that we want to chec
+function checkRequired(inputArray) {
+  inputArray.forEach(function(input) {
+    // console.log(input.value)
+    // use the .trim below to remove white space
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`)
+    } else {
+      showSuccess(input)
+    }
+  })
+}
+
+// get field name and uppercase the first letter use slice to cut off the first letter since we have that coming in from charAt(0), add the rest of the word with the + input.id.slice(1)
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
 // event listeners below the functions
 form.addEventListener('submit', function(e){
   e.preventDefault();
-  if(username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSuccess(username);
-  }
-  if(email.value === '') {
-    showError(email, 'Email is required');
-  } else if(!isValidEmail(email.value)){
-    showError(email, 'Email is not valid');
-  } else {
-    showSuccess(username);
-  }
-  if(password.value === '') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(username);
-  }
-  if(password2.value === '') {
-    showError(password2, 'Password2 is required');
-  } else {
-    showSuccess(username);
-  }
+  // if(username.value === '') {
+  //   showError(username, 'Username is required');
+  // } else {
+  //   showSuccess(username);
+  // }
+  // if(email.value === '') {
+  //   showError(email, 'Email is required');
+  // } else if(!isValidEmail(email.value)){
+  //   showError(email, 'Email is not valid');
+  // } else {
+  //   showSuccess(username);
+  // }
+  // if(password.value === '') {
+  //   showError(password, 'Password is required');
+  // } else {
+  //   showSuccess(username);
+  // }
+  // if(password2.value === '') {
+  //   showError(password2, 'Password2 is required');
+  // } else {
+  //   showSuccess(username);
+  // }
+  checkRequired([username, email, password, password2])
 
 })
