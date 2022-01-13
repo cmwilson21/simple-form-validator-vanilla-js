28cdec3b2e7d9130708e80a7c860e364b3330946
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-01-12 21:51:35 -0600

    Cleaned up the get email function

--- script.js
+++ script.js
@@ -1,49 +1,71 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
 // show input error message
 function showError(input, message) {
   const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText = message
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
-// show success outline 
+// show success outline
 function showSuccess(input) {
   const formControl = input.parentElement;
-  formControl.className = 'form-control success';
+  formControl.className = "form-control success";
 }
 
-// check email is valid 
-function isValidEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+// check email is valid
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input)) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 
 // check required fields - pass in array of inputs that we want to chec
 function checkRequired(inputArray) {
-  inputArray.forEach(function(input) {
+  inputArray.forEach(function (input) {
     // console.log(input.value)
     // use the .trim below to remove white space
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`)
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
     } else {
-      showSuccess(input)
+      showSuccess(input);
     }
-  })
+  });
 }
 
 // get field name and uppercase the first letter use slice to cut off the first letter since we have that coming in from charAt(0), add the rest of the word with the + input.id.slice(1)
 function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
 }
 
 // event listeners below the functions
-form.addEventListener('submit', function(e){
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   // if(username.value === '') {
   //   showError(username, 'Username is required');
@@ -67,6 +89,9 @@ form.addEventListener('submit', function(e){
   // } else {
   //   showSuccess(username);
   // }
-  checkRequired([username, email, password, password2])
-
-})
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  // checkLength checks the field, the minium, and the max - we built this above
+  checkLength(password, 6, 25);
+  checkEmail(email);
+});
